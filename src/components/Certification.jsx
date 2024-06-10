ee7402cd76d2b8bbfe16bf901ca0d2c6d6627cf9
Samiul Mushfik <samiulmushfik123@gmail.com>
2024-06-09 21:30:06 -0500

    Make certifications component responsive

--- src/components/Certification.jsx
+++ src/components/Certification.jsx
@@ -1,11 +1,31 @@
-import {certificationBg} from "../assets";
-import React from "react";
-import {certifications} from "../constants";
-import {motion} from "framer-motion";
-import {fadeIn} from "../utils/motion";
-import {styles} from "../styles";
+import { certificationBg } from "../assets";
+import React, { useEffect, useState } from "react";
+import {certifications, skillGroups} from "../constants";
+import { motion } from "framer-motion";
+import { fadeIn } from "../utils/motion";
+import { styles } from "../styles";
+import { Slide } from "react-slideshow-image";
+import 'react-slideshow-image/dist/styles.css';
+import { chunkArray } from "../utils/helpers";
 
 export const Certification = (props) => {
+    const [chunkedGroups, setChunkedGroups] = useState([]);
+    const [chunkSize, setChunkSize] = useState(0);
+
+    const handleResize = () => {
+        let chunkSize = 1;
+        if (window.innerWidth >= 1024) chunkSize = 3;
+        // else if (window.innerWidth >= 640 && window.innerWidth < 1024) chunkSize = 2;
+        setChunkSize(chunkSize);
+        setChunkedGroups(chunkArray(certifications, chunkSize));
+    };
+
+    useEffect(() => {
+        handleResize();
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
     return (
         <div
             id={props.id}
@@ -16,26 +36,43 @@ export const Certification = (props) => {
             <div className="absolute inset-0 bg-custom-gray opacity-80 z-0"></div>
             <h2 className={styles.pageTitle}>Certifications</h2>
             <motion.div
-                initial="hidden"
-                whileInView="show"
-                viewport={{ once: false, amount: 0.25 }}
-                className="m-auto flex lg:flex-row md:flex-row flex-col gap-5">
-            {
-                certifications.map((certification, index) => (
-                    <motion.div
-                        className="flex-1 rounded-lg items-center z-10"
-                        whileHover={{scale: 1.1}}
-                        variants={fadeIn('right', 'spring', index * 0.5, 0.75)}>
-                            <a className="hover:cursor-pointer"
-                               onClick={() => window.open(certification.credentialUrl)}>
-                                <img
-                                    src={certification.image}
-                                    alt={certification.issuer}
-                                    className="w-full h-full rounded-lg"/>
-                            </a>
-                    </motion.div>
-                ))
-            }
+                    initial="hidden"
+                    whileInView="show"
+                    viewport={{ once: false, amount: 0.25 }}
+                    className="relative z-10 w-full lg:w-screen">
+                <Slide arrows={false}
+                       indicators={chunkSize < certifications.length}
+                       autoplay={chunkSize < certifications.length}
+                       canSwipe={chunkSize < certifications.length}
+                >
+                    {
+                        chunkedGroups.map((group, groupIndex) => (
+                            <div key={groupIndex} className="flex flex-wrap w-full">
+                                {
+                                    group.map((item, itemIndex) => (
+                                        <motion.div
+                                            key={itemIndex}
+                                            className="flex-1 rounded-lg items-center z-10 p-3"
+                                            whileHover={{ scale: 1.1 }}
+                                            variants={fadeIn('right', 'spring', itemIndex * 0.5, 0.75)}
+                                        >
+                                            <a
+                                                className="hover:cursor-pointer"
+                                                onClick={() => window.open(item.credentialUrl)}
+                                            >
+                                                <img
+                                                    src={item.image}
+                                                    alt={item.issuer}
+                                                    className="w-full h-auto rounded-lg"
+                                                />
+                                            </a>
+                                        </motion.div>
+                                    ))
+                                }
+                            </div>
+                        ))
+                    }
+                </Slide>
             </motion.div>
         </div>
     )
